6dc3931ab764797fc93c1796c95296784821469e
sho0318 <sho.ishii.0318@gmail.com>
2026-03-01 18:19:33 +0900

    IPアドレスを動的に割り振る

--- internal/network/networkManager.go
+++ internal/network/networkManager.go
@@ -3,9 +3,13 @@ package network
 import (
 	"fmt"
 	"strconv"
-
+	"os"
+    "path/filepath"
 	"gocker/internal"
+	"strings"
 )
+
+const ipDbPath = "/var/run/gocker/ips"
 
 type NetworkManager struct {
 	config *internal.Config
@@ -46,4 +50,32 @@ func (nm *NetworkManager) ConfigureNamespace(pid int, ifName string) {
 
     internal.Run("nsenter", "-t", pidStr, "-n", "sysctl", "-w", "net.ipv4.conf.all.accept_redirects=0")
     internal.Run("nsenter", "-t", pidStr, "-n", "sysctl", "-w", "net.ipv4.conf.eth0.accept_redirects=0")
+}
+
+func (nm *NetworkManager) AllocateIP() string {
+	if err := os.MkdirAll(ipDbPath, 0755); err != nil {
+        panic(fmt.Errorf("failed to create IP database directory: %v", err))
+    }
+
+	for i := 2; i < 254; i++ {
+		ip := fmt.Sprintf("172.18.0.%d", i)
+		recordFile := filepath.Join(ipDbPath, ip)
+
+		if _, err := os.Stat(recordFile); os.IsNotExist(err) {
+            if err := os.WriteFile(recordFile, []byte(""), 0644); err != nil {
+                panic(err)
+            }
+            return ip
+        }
+	}
+	panic("No more IP address available")
+}
+
+func (nm *NetworkManager) ReleaseIP(ip string) {
+    cleanIP := strings.Split(ip, "/")[0]
+    recordFile := filepath.Join(ipDbPath, cleanIP)
+    
+    if err := os.Remove(recordFile); err != nil {
+        fmt.Printf("Failed to release IP: %v\n", err)
+    }
 }
